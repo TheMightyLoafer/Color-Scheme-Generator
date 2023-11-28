54d2cef1d98df884c40088f4d531b3d7670b206a
TheMightyLoafer <147700520+TheMightyLoafer@users.noreply.github.com>
2023-11-28 07:39:21 -0800

    update index.js
    Color API now succesfully sends data back after request

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const requestSchemeBtn = document.getElementById("get-scheme")
+
+requestSchemeBtn.addEventListener('click', () => {
+    const colorPickerEl = document.getElementById('color-picker').value;
+    const selectSchemeEl = document.getElementById('color-dropdown').value;
+    const colorAPI = "https://www.thecolorapi.com/id";
+    
+
+    const params = new URLSearchParams({
+        hex: colorPickerEl.replace('#', ''),
+        mode: selectSchemeEl,
+    });
+
+    const url = `${colorAPI}?${params}`;
+
+    console.log(url);
+    console.log(colorPickerEl.value);
+    console.log(selectSchemeEl.value);
+    console.log(params)
+    console.log(url);
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+});
